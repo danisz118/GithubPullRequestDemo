00042ee53581f2a230df33a29aa5da2caa028202
danisz118 <danisz118@gmail.com>
2020-03-13 09:10:47 +0200

    change commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-        // show me you Stream API power
-        System.out.println("test");
+        IntStream.of(50, 60, 70, 80, 90, 100, 110, 120).filter(x -> x < 90).map(x -> x + 10)
+                .limit(3).forEach(x-> System.out.println(x));
     }
 
 }
